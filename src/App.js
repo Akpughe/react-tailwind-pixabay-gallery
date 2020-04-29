5b0fc216a51d1be7349446cc3f12b8a88ab0a789
David <davidakpughe2@gmail.com>
2020-04-29 12:04:51 +0100

    Searching the api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from './components/imageCard';
+import Search from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,14 +17,22 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
   return (
     <div className="container mx-auto">
-     {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <Card key = {image.id} image={image} />
-        ))}
-      </div>}
+      <Search searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length == 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <Card key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
